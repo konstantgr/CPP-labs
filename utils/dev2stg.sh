982bc740aef6012c392a8a56c4e3942ae6e78efa
konstantgr <60608301+konstantgr@users.noreply.github.com>
2022-03-05 00:25:28 +0300

    refactoring

--- utils/dev2stg.sh
+++ utils/dev2stg.sh
@@ -1,15 +1,26 @@
+cd ..
 git checkout stg
+
+echo "======================"
 echo "Merging develop branch"
+echo "======================"
+
 git merge dev
 
 timestamp=$(date +%s)
+echo "======================"
 echo "Tagging as ${timestamp}"
+echo "======================"
 git tag -a "$timestamp" -m "dev2stg"
 
+echo "======================"
 echo "Pushing commits and tags"
+echo "======================"
 git push --set-upstream origin stg
 git push --tags
 
+echo "======================"
 echo "Checking out develop branch"
+echo "======================"
 git checkout dev
 git push
