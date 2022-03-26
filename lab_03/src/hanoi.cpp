de04b743140d9b93a1928a9102ae5d61b5d26769
konstantgr <60608301+konstantgr@users.noreply.github.com>
2022-03-26 11:16:41 +0300

    Hanoi with generics

--- lab_03/src/hanoi.cpp
+++ lab_03/src/hanoi.cpp
@@ -5,7 +5,62 @@ using i64 = long long;
 using namespace std;
 
 
-vector<string> hanoi(i64 n, int i, int k, vector<string> res) {
+struct Node {
+    string val;
+    Node* next;
+
+    Node(string _val) : val(_val), next(nullptr) {}
+};
+
+struct list {
+    Node* first;
+    Node* last;
+
+    list() : first(nullptr), last(nullptr) {}
+
+    bool is_empty() {
+        return first == nullptr;
+    }
+
+    void push_back(string _val) {
+        Node* p = new Node(_val);
+        
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+        last->next = p;
+        last = p;
+    }
+
+    void print() {
+        if (is_empty()) {
+            cout << "Empty list"; 
+            return;
+        }
+        Node* p = first;
+
+        while (p) {
+            cout << p->val << endl;
+            p = p->next;
+        }
+    }
+
+    void clear() {
+        Node* p = first;
+
+        while (p) {
+            cout << p->val << " deleted" << endl;
+            delete p;
+            p = p->next;
+        }
+    }
+};
+
+
+template <typename T>
+T hanoi_generic(i64 n, int i, int k, T res) {
     int tmp;
 
     if (n == 1) {
@@ -14,9 +69,9 @@ vector<string> hanoi(i64 n, int i, int k, vector<string> res) {
     }
     else {
         tmp = 6 - i - k;
-        res = hanoi(n - 1, i, tmp, res);
+        res = hanoi_generic(n - 1, i, tmp, res);
         res.push_back("Move disk " + to_string(n) + " from pin " + to_string(i) + " to " + to_string(k));
-        res = hanoi(n - 1, tmp, k, res);
+        res = hanoi_generic(n - 1, tmp, k, res);
         return res;
     }
 }
@@ -25,9 +80,19 @@ int main(int argc, char** argv)
 {
     i64 n = 3;
     vector<string> res;
-    res = hanoi(n, 1, 2, res);
+    list lst;
+
+    // res = hanoi(n, 1, 2, res);
+    // lst = hanoi_list(n, 1, 2, lst);
+
+    res = hanoi_generic(n, 1, 2, res);
+    lst = hanoi_generic(n, 1, 2, lst);
 
     for (auto i: res) cout << i << endl;
+    cout << endl;
+    lst.print();
+    lst.clear();
 
     return 0;
 }
+
